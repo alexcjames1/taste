7846094173cd03dac788e2bcb00a96e04309a8a7
Alex James <alexcjames1@gmail.com>
2020-04-16 15:40:06 -0500

    finished adding content to the entertainment page

--- src/components/EntertainmentList/EntertainmentList.jsx
+++ src/components/EntertainmentList/EntertainmentList.jsx
@@ -17,12 +17,12 @@ export default class EntertainmentList extends Component {
                     <Col xs="12" sm="8" className={style.heroContent}>
                         <h2 className={style.heading}>Our Entertainment</h2>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna
-                            aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                            ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            Duis aute irure dolor in reprehenderit in voluptate velit
-                            esse cillum
+                            Our Entertainment pick for the 2020 season has been
+                            hand selected by the Taste Wine Fest organizers
+                            based on feedback from attendees from previous wine
+                            fest seasons. If you would like to suggest
+                            entertainment for our next event, follow us
+                            on Facebook!
                         </p>
                     </Col>
                 </Row>
@@ -36,12 +36,12 @@ export default class EntertainmentList extends Component {
                         </div>
                         <div className={style.content}>
                             <p className={style.body}>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore magna
-                                aliqua. Consectetur purus ut faucibus pulvinar elementum
-                                integer. Risus viverra adipiscing at in tellus integer
-                                feugiat scelerisque varius. Dictum at tempor commodo
-                                ullamcorper elementum.
+                                The Captain Miller Band is a group local to the
+                                Chicago area and formed in 2011. They have
+                                performed live over 300 times and have sold more
+                                than 3,500 albums. The Captain Miller Band is
+                                known for their unique rock-pop sound in the hits
+                                "Sock Pasta" and "What The Heck, Man."
                             </p>
                         </div>
                     </Col>
