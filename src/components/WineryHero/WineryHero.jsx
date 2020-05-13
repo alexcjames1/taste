4b035eeb6abe5e47f954642b78f9603453567c23
Alex James <alexcjames1@gmail.com>
2020-05-13 16:28:05 -0500

    added fade in animation to landing page hero content

--- src/components/WineryHero/WineryHero.jsx
+++ src/components/WineryHero/WineryHero.jsx
@@ -13,7 +13,7 @@ export default class WineryHero extends Component {
         return(
             <Section className={style.section} kind="secondary" size="normal">
                 <Row>
-                    <Col xs="12" sm="4" className={style.heroContent}>
+                    <Col xs="9" sm="4" className={style.heroContent}>
                         <h1 className={style.heading}>Our Winery Family</h1>
                         <p className={style.body}>
                             The Taste Wine Fest showcases a variety of wines
